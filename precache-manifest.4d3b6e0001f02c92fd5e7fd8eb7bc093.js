08d51b62d7dd91fa05c522d9e4269cff5f60ef78
Alexandria Eicher <alexandriaeicher@gmail.com>
2020-05-08 17:07:33 -0400

    Updates

---
+++ precache-manifest.4d3b6e0001f02c92fd5e7fd8eb7bc093.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652ac50c1c87507afccaa821a512d0c0",
+    "url": "/joshuakoch.github.io/index.html"
+  },
+  {
+    "revision": "b5ea9cbbe8438e642409",
+    "url": "/joshuakoch.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7e59b9a2655ac9083f10",
+    "url": "/joshuakoch.github.io/static/js/2.e1bb7d60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/joshuakoch.github.io/static/js/2.e1bb7d60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5ea9cbbe8438e642409",
+    "url": "/joshuakoch.github.io/static/js/main.f2cb28ed.chunk.js"
+  },
+  {
+    "revision": "ed38a84de806bbdaa0f6",
+    "url": "/joshuakoch.github.io/static/js/runtime-main.0e0c1af6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/joshuakoch.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/joshuarkoch.github.io/precache-manifest.89934850ad6cc71cf24b529652f0d56c.js"
+  "/joshuakoch.github.io/precache-manifest.4d3b6e0001f02c92fd5e7fd8eb7bc093.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/joshuarkoch.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/joshuakoch.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
